deed18ff8d65fcc2b3580160f30d7be0e3583fc2
Adrian <aacedeno23@gmail.com>
2022-12-23 15:27:24 +0000

    Basic tf scripts

--- Terraform-Docker/main.tf
+++ Terraform-Docker/main.tf
@@ -15,7 +15,7 @@ resource "docker_image" "nodered_image" {
 
 #Creating a random combination of characters to give unique suffix to the 1st container
 resource "random_string" "random" {
-  count   = 2 #makes 2 random_string resource
+  count   = var.container_count #makes 2 random_string resource
   length  = 4
   special = false
   upper   = false
@@ -23,24 +23,13 @@ resource "random_string" "random" {
 
 #name is a logiclly value in this instance, just need to use it for referecning
 resource "docker_container" "nodered_container" {
-  count = 2
+  count = var.container_count
   name  = join("-", ["nodered", random_string.random[count.index].result]) #using join function to assign the random characters to container
   image = docker_image.nodered_image.latest                                #Image needs to be referenced the image resource
   ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
   }
 
 }
 
-#Outputs the name for all nodered_containers created 
-output "container-name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
-}
-
-#Outputs the IP address and external ports for each nodered_container created
-output "ip-address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
-  description = "The IP address and external port of the container"
-}

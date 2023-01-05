558a1d21e56da77dd5c63fcc91b8a44548180223
Adrian <aacedeno23@gmail.com>
2023-01-05 02:44:18 +0000

    Tf module code

--- Terraform-Docker/container/variables.tf
+++ Terraform-Docker/container/variables.tf
@@ -1,18 +1,6 @@
-variable "ext_port_in" {
-    description = "External port for container"
-}
-
-variable "int_port_in" {
-    description = "Internal port for container"
-}
-
-variable "image_in" {
-    description = "Image of container" 
-}
-
-variable "name_in" {
-    description = "Naming schema of container"
-}
-
+variable "ext_port_in" {}
+variable "int_port_in" {}
+variable "image_in" {}
+variable "name_in" {}
 variable "container_path_in" {}
- variable "host_path_in" {}
+variable "host_path_in" {}
